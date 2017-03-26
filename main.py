c59bd8f2b74b6179b1d1017f0c09b417932770e6
nico-iaco <nicolaiacovelli98@gmail.com>
2017-03-26 09:35:02 +0200

    Miglioramenti vari

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 import time
 
-PROXY = "122.128.109.149:80"
+PROXY = "122.128.109.149:80"        #Funziona solo per le richieste http e non per quelle https
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--proxy-server=http://%s' % PROXY)
-
+n = int(input('Inserisci numero visite da generare:'))
+sito = str(input('Inserisci URL sito da visualizzare:'))
+i = 0
 chrome = webdriver.Chrome(chrome_options=chrome_options)
-chrome.get("http://whatismyipaddress.com")
-time.sleep(20)
-chrome.get("http://facebook.com")
+while i < n:
+    chrome.get(sito)
+    print('Richiesta eseguita')
+    i += 1
+    time.sleep(20)
